4381c665d05ae5c634193e5832214dffb44503c3
itIsMuse <oyeniranshock@gmail.com>
2026-08-03 15:09:18 +0100

    chore: update eslint-config-next to version 16.2.12

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+
   {
     ignores: [
       "node_modules/**",
@@ -21,5 +29,3 @@ const eslintConfig = [
     ],
   },
 ];
-
-export default eslintConfig;
